f026c3932658f0f5171a35b5269bd40b7d8e1b17
oureveryday <oureveryday@163.com>
2023-09-10 10:16:31 +0800

    Crack

---
+++ src/Patch.md
@@ -0,0 +1,18 @@
+# Crack Info
+
+## PatchLauncher
+
+Automacally write launch info in Baymax64.ini and Launches Baymax Patch.
+
+## Crack.BPT
+
+Baymax Patch Tools project file.
+
+* Search: `E8 3F 02 FE FF 48 8B F0`
+* Replace: `48 C7 C6 01 00 00 00 90`
+
+---
+
+* Breakpoint: `FF 15 7F 5F 12 00`
+* r8: Launch Info (String format `C:\cheat<@/>1<@/>000000000000000000<@/>Crackkkk`)
+* r9,r13,r14: Launch Info String Length
---
+++ src/PatchLauncher/PatchLauncher/PatchLauncher.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <windows.h>
+
+void RunExe(const char* cmdline) {
+    STARTUPINFO si = { sizeof(STARTUPINFO) };
+    PROCESS_INFORMATION pi;
+
+    size_t len = strlen(cmdline) + 1;
+    wchar_t* w_cmdline = new wchar_t[len];
+
+    size_t out_len;
+    mbstowcs_s(&out_len, w_cmdline, len, cmdline, len - 1);
+
+    if (CreateProcessW(NULL, w_cmdline, NULL, NULL,
+        FALSE, 0, NULL, NULL, &si, &pi)) {
+        return;
+    }
+    else {
+        std::cout << "Failed to run command " << cmdline << ", error: " << GetLastError() << std::endl;
+    }
+
+    delete[] w_cmdline;
+}
+
+std::string GetCurrentWorkingDir() {
+    char current_dir[MAX_PATH];
+    if (!GetCurrentDirectoryA(MAX_PATH, current_dir)) {
+        std::cerr << "Error getting current directory: #" << GetLastError();
+    }
+    return std::string(current_dir);
+}
+
+
+int main() {
+    std::string data_string = GetCurrentWorkingDir() + "<@/>1<@/>000000000000000000<@/>Crackkkk";
+    std::string file = "Baymax64.Ini";
+    std::string patcher = "Patch.ex_";
+
+    int size = data_string.size() + 1;
+
+    SetFileAttributesA(file.c_str(), FILE_ATTRIBUTE_NORMAL);
+
+    if (std::ifstream(file))
+        std::remove(file.c_str());
+
+    std::ofstream ofs(file);
+    if (!ofs) {
+        std::cout << "Failed to open file\n";
+        return -1;
+    }
+    ofs << "[BAYMAX64]" << "\n";
+    ofs << "DATA = " << data_string << "\n";
+    ofs << "SIZE = " << size << "\n";
+    ofs.close();
+
+    SetFileAttributesA(file.c_str(), FILE_ATTRIBUTE_READONLY);
+
+    RunExe(patcher.c_str());
+
+    return 0;
+}
